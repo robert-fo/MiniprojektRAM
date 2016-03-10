ddd2d8fb5f89d0c0773050d41c547fab9b74d6ee
annhuh <huhta@hotmail.se>
2016-03-10 10:48:58 +0100

    Data annotations added

--- MiniprojektRAM/Models/QuestionCategory.cs
+++ MiniprojektRAM/Models/QuestionCategory.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +9,10 @@ namespace MiniprojektRAM.Models
 {
     public class QuestionCategory
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [Display(Name="Kategori")]
         public int CatName { get; set; }
     }
 }
