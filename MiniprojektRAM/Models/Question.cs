449f96e31614f08df07461e03824019b1e522e44
lindm <lind005@gmail.com>
2016-03-10 10:18:35 +0100

    added class Question

---
+++ MiniprojektRAM/Models/Question.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MiniprojektRAM.Models
+{
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int cId { get; set; }
+
+        [Required]
+        public string Question { get; set; }
+
+        [Required]
+        public string Answer { get; set; }
+
+        [ForeignKey("cId")]
+        public virtual QuestionCategory questionCategory { get; set; }
+
+    }
+}
