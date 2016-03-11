52b39d9066ab0671fa6619cdffdde4fc171afce7
robert-fo <robert.fo@gmail.com>
2016-03-11 13:19:15 +0100

    Fungerande ColorQuiz 2

--- MiniprojektRAM/ViewModels/QuestionViewModel.cs
+++ MiniprojektRAM/ViewModels/QuestionViewModel.cs
@@ -6,6 +6,9 @@ using System.Web;
 
 namespace MiniprojektRAM.ViewModels
 {
+    //När man genererar en edit view med denna som modell så vill Vs lägga till det i databasen,
+    //så ta bort raden för denna klass i DataAccessLayer/ItemContext.cs filen efter att ni genererat
+    //en vy med denna klass som modellklass eftersom denna ska vara helt frikopplad från databasen.
     public class QuestionViewModel
     {
         public int Id { get; set; }
